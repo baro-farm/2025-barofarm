8e8bb6824b1a6e5755f22ea86a5a955805301e91
s0ycraft <s0ycr4ft@gmail.com>
2025-04-10 14:23:45 +0900

    soy branch

--- src/main/java/vo/SellerVO.java
+++ src/main/java/vo/SellerVO.java
@@ -2,6 +2,7 @@ package vo;
 
 public class SellerVO {
     // user 테이블 정보
+	private String userNum;
     private String userId;
     private String userName;
     private String phone;
@@ -11,6 +12,7 @@ public class SellerVO {
     private boolean deleted;
 
     // sellerDetail 테이블 정보
+    private String sellerNum;
     private String storeName;
     private String businessNum;
     private boolean isAlarm;
