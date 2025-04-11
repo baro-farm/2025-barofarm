f2ad88bd28c2c497f0d3440a0e75ee1191ab2c7c
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-04-11 17:47:44 +0900

    공지사항 목록

---
+++ src/main/java/service/admin/NoticeService.java
@@ -0,0 +1,9 @@
+package service.admin;
+
+import java.util.List;
+
+import dto.admin.Notice;
+
+public interface NoticeService {
+	List<Notice> allNotice() throws Exception;
+}
---
+++ src/main/webapp/admin/noticeList.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('#notie_table').DataTable({
+    colReorder: true,
+    responsive: true,
+    scrollX: true,
+  });
+});
